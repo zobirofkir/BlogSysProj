b9ff9236a2af130a06255967a3aee71ae996102a
zobirofkir <zobirofkir19@gmail.com>
2024-06-02 11:33:02 +0100

    Create Authenticated Routes

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource("users", UserController::class);
-Route::apiResource("users.posts", PostController::class);
 Route::post("login", [AuthController::class, "login"]);
-Route::post("logout", [AuthController::class, "logout"]);
+
+Route::middleware("auth:api")->group(function(){
+    Route::apiResource("users.posts", PostController::class);
+    Route::post("logout", [AuthController::class, "logout"]);
+});
--- tests/Feature/PostRequestTest.php
+++ tests/Feature/PostRequestTest.php
@@ -6,16 +6,24 @@ use App\Models\Post;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Passport\Passport;
 use Tests\TestCase;
 
 class PostRequestTest extends TestCase
 {
+
+    public function authenticated()
+    {
+        $user = User::factory()->create();
+        Passport::actingAs($user);
+    }
     /**
      * A basic feature test example.
     */
 
     public function testGetPosts()
     {
+        $this->authenticated();
         $user = User::factory()->create();
         Post::factory()->count(4)->create(['user_id' => $user->id]);
 
